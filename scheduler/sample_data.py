bdf9cb149eeff6ecfa280d1e9b7455bd0f1dac51
Arjun Tyagi <arjuntyagi1920@gmail.com>
2026-08-11 03:30:30 +0530

    Add files via upload

---
+++ scheduler/sample_data.py
@@ -0,0 +1,19 @@
+faculty_allocations = [
+    {
+        "faculty_id": "F001",
+        "faculty_name": "Dr. Prasanna",
+        "subject_code": "21CSC302J",
+        "subject_name": "Computer Networks",
+        "slot": "A",
+        "batch": 1
+    },
+
+    {
+        "faculty_id": "F001",
+        "faculty_name": "Dr. Prasanna",
+        "subject_code": "21CSC307P",
+        "subject_name": "Machine Learning for Data Analytics",
+        "slot": "F",
+        "batch": 2
+    }
+]
